62906e8a4831d880c4338d1b3e60ac69e2f7c51b
asuka1211 <maks1211@bk.ru>
2019-03-21 00:20:44 +0400

    BUG FIX, translate back to russian

--- app/src/main/java/com/serma/crocobisunes/timetable/activity_grid_item.java
+++ app/src/main/java/com/serma/crocobisunes/timetable/activity_grid_item.java
@@ -17,6 +17,7 @@ public class activity_grid_item extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_item);
+        hideNavigator();
         imageView = findViewById(R.id.imageView);
         Intent intent = getIntent();
         int receivedImage = intent.getIntExtra("image",0);
@@ -38,5 +39,12 @@ public class activity_grid_item extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void hideNavigator(){
+        View decorView =getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
